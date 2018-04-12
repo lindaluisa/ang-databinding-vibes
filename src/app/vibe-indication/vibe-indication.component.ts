fd4b9c728e9d28004f49f0b7fa3c16fe131dcd5f
lindaluisa <lindaluisarosenthal@gmail.com>
2018-04-12 13:53:41 +0200

    implementing @ViewChild to access template via reference variable

--- src/app/vibe-indication/vibe-indication.component.ts
+++ src/app/vibe-indication/vibe-indication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-vibe-indication',
@@ -9,26 +9,32 @@ export class VibeIndicationComponent implements OnInit {
   @Output() goodVibeCreated = new EventEmitter<{goodVibeName: string, goodVibeContent: string}>();
   @Output() badVibeCreated = new EventEmitter<{badVibeName: string, badVibeContent: string}>();
   // newVibeName = '';
-  newVibeContent = '';
+  // newVibeContent = '';
+  @ViewChild('vibeContentInput') vibeContentInput: ElementRef;
+  // Element Ref is an Angular type; 
+  // Element Ref is a reference to a particular element
+  // has a useful property --> nativeElement 
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  // HTMLInputElement makes user that value property is recognized
+  // HTMLInputElement makes user that .value property is recognized
+  // nativeElement gives access to underlying element
+  // .value still comes from input
+  // inside method: console.log(nameInput); console.log(this.vibeContentInput)
   onAddGoodVibe(nameInput: HTMLInputElement) {
-    // console.log(nameInput);
     this.goodVibeCreated.emit({
       goodVibeName: nameInput.value,
-      goodVibeContent: this.newVibeContent
-    });
+      goodVibeContent: this.vibeContentInput.nativeElement.value
+  });
   }
 
   onAddBadVibe(nameInput: HTMLInputElement) {
     this.badVibeCreated.emit({
       badVibeName: nameInput.value,
-      badVibeContent: this.newVibeContent
+      badVibeContent: this.vibeContentInput.nativeElement.value
     })
   }
 
